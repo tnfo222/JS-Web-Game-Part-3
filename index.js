f316c451b8a588a449378d6f39183a32dfe205ff
tnfo222 <github.com/tnfo222/psychic-disco>
2022-05-08 12:55:35 -0400

    move character by calling on function to:

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
-function newImage(url, left, bottom){
+function newImage(url){
     let image = document.createElement('img')
     image.src = url
-    image.style.position = 'fixed'
-    image.style.left = left + 'px'
-    image.style.bottom = bottom + 'px'
     document.body.append(image)
     return image
 }
 
-newImage('assets/green-character.gif', 100, 250)
-newImage('assets/tree.png', 200, 450)
-newImage('assets/pillar.png', 350, 250)
-newImage('assets/pine-tree.png', 450, 350)
-newImage('assets/crate.png', 150, 350)
-newImage('assets/well.png', 500, 575)
+function move(image){
+    image.style.position = 'fixed'
+
+    function moveToCoordinates(left,bottom){
+        image.style.left = left + 'px'
+        image.style.bottom = bottom + 'px'
+    }
+    return {
+        to: moveToCoordinates
+    }
+}
+
+move(newImage('assets/green-character.gif')).to(100, 250)
+//newImage('assets/green-character.gif', 100, 250)
+//newImage('assets/tree.png', 200, 450)
+//newImage('assets/pillar.png', 350, 250)
+//newImage('assets/pine-tree.png', 450, 350)
+//newImage('assets/crate.png', 150, 350)
+//newImage('assets/well.png', 500, 575)
 
 
 function newItem(url, left, bottom){
@@ -27,9 +37,9 @@ function newItem(url, left, bottom){
     return item
 }
 
-newItem('assets/sword.png', 500, 555)
-newItem('assets/shield.png', 165, 335)
-newItem('assets/staff.png', 600, 250)
+//newItem('assets/sword.png', 500, 555)
+//newItem('assets/shield.png', 165, 335)
+//newItem('assets/staff.png', 600, 250)
 
 function newInventory(){
     let inventory = document.createElement('div')
